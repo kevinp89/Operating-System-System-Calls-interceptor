a3f02cbf5440537a65a861c7c2903bf58a8d79ef
root <root@localhost.localdomain>
2017-08-02 13:51:25 -0400

    added the necessary files

---
+++ interceptor.h
@@ -0,0 +1,25 @@
+#ifndef _COMMON_H
+#define _COMMON_H
+
+#define REQUEST_SYSCALL_INTERCEPT       1
+#define REQUEST_SYSCALL_RELEASE         2
+#define REQUEST_START_MONITORING        3
+#define REQUEST_STOP_MONITORING         4
+
+#define MY_CUSTOM_SYSCALL               0
+
+#ifdef __KERNEL__
+
+int error_check(int cmd, int syscall, int pid);
+long root_user_check(void);
+asmlinkage long my_syscall(int cmd, int syscall, int pid);
+
+#define log_message(pid, syscall, arg1, arg2, arg3, arg4, arg5, arg6) \
+	printk(KERN_DEBUG "[%x]%lx(%lx,%lx,%lx,%lx,%lx,%lx)\n", pid, \
+		syscall, \
+		arg1, arg2, arg3, arg4, arg5, arg6 \
+	);
+#endif
+
+
+#endif /* _COMMON_H */
